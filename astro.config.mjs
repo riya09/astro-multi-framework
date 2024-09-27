99140c81b4ba07708ca054b6de61b594bb6217b7
Faria Begum Riya <riya09@users.noreply.github.com>
2024-09-27 22:42:28 +0600

    feat: add tailwind in project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react"
 import vue from "@astrojs/vue"
 
+import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
-  integrations: [react(), vue()],
+  integrations: [react(), vue(), tailwind()],
 })
