921f98fe43ef877a3765f31b723aac10062af5fc
Faria Begum Riya <riya09@users.noreply.github.com>
2024-09-28 23:13:19 +0600

    style: add css classes on cart

--- src/components/vue/Cart.vue
+++ src/components/vue/Cart.vue
@@ -1,45 +1,75 @@
 <template>
   <div>
-    <div
-      class="cart-icon"
+    <button
+      class="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full
+      hover:text-gray-400 focus:outline-none"
+      aria-label="Cart"
       @click="toggleList"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="24" height="24">
-        <path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6
-        50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488
-        336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4
-        54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48
-        0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
-      </svg>
-      <span v-if="total > 0">{{ total }}</span>
-    </div>
-    <div
-      v-if="showList"
-      class="item-list"
+      <svg
+        class="h-6 w-6"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
       >
-      <ul>
-        <li v-for="(item, i) in cartItems" :key="i">
-          {{ item.name }}
-        </li>
-      </ul>
+        <path
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        ></path>
+      </svg>
+      <span v-if="total > 0" class="absolute inset-0 object-right-top -mr-6">
+        <span
+          class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs
+          font-semibold leading-4 bg-red-500 text-white"
+        >
+          {{ total }}
+        </span>
+      </span>
+    </button>
+    <div v-if="showList" class="border-2 border-gray-200 bg-red p-1 rounded-md w-72">
+      <template v-for="(item, i) in cartItems" :key="i">
+        <div class="flex items-center justify-between mb-1">
+          <h5 class="font-manrope font-bold text-md leading-9 text-gray-900">
+             {{ item.name }}
+          </h5>
+          <button class="rounded-full group flex items-center justify-center focus-within:outline-red-500">
+              <svg
+                width="34"
+                height="34"
+                viewBox="0 0 34 34"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <circle class="fill-red-50 transition-all duration-500 group-hover:fill-red-400"
+                      cx="17" cy="17" r="17" fill="" />
+                <path class="stroke-red-500 transition-all duration-500 group-hover:stroke-white"
+                    d="M14.1673 13.5997V12.5923C14.1673 11.8968 14.7311 11.333 15.4266 11.333H18.5747C19.2702 11.333 19.834 11.8968 19.834 12.5923V13.5997M19.834 13.5997C19.834 13.5997 14.6534 13.5997 11.334 13.5997C6.90804 13.5998 27.0933 13.5998 22.6673 13.5997C21.5608 13.5997 19.834 13.5997 19.834 13.5997ZM12.4673 13.5997H21.534V18.8886C21.534 20.6695 21.534 21.5599 20.9807 22.1131C20.4275 22.6664 19.5371 22.6664 17.7562 22.6664H16.2451C14.4642 22.6664 13.5738 22.6664 13.0206 22.1131C12.4673 21.5599 12.4673 20.6695 12.4673 18.8886V13.5997Z"
+                    stroke="#EF4444" stroke-width="1.6" stroke-linecap="round" />
+              </svg>
+          </button>
+        </div>
+        <h6 class="text-indigo-600 font-manrope font-bold text-md leading-9 text-right">
+          ${{ item.price }}
+        </h6>
+      </template>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useStore } from '@nanostores/vue'
-import { ref } from 'vue'
-import { totalItem, items } from '../../store'
+import { useStore } from "@nanostores/vue";
+import { ref } from "vue";
+import { totalItem, items } from "../../store";
 
-const total = useStore(totalItem)
-const cartItems = useStore(items)
-const showList = ref(false)
+const total = useStore(totalItem);
+const cartItems = useStore(items);
+const showList = ref(false);
 
 const toggleList = () => {
-  showList.value = !showList.value
-}
+  showList.value = !showList.value;
+};
 </script>
 
 <style>
-
 </style>
